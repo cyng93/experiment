587caca73f6a90cb3214c87adf994ab2c27a8051
cyng93 <cyng93@gmail.com>
2017-12-15 12:09:44 +0000

    Add initNewLKM tool

---
+++ initNewLKM.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+die()
+{
+  echo "$1"
+  exit 1
+}
+
+usage()
+{
+  prog=$(basename $0)
+  echo "Usage: $prog {proj_name}"
+  die ""
+}
+
+
+#
+# Entry Point
+#
+proj=$1
+
+if [ "$#" -lt 1 ]; then
+  usage
+fi
+
+
+if [ -d "$proj" ]; then
+  die "directory '$proj' existed, try a new name?"
+fi
+
+cp -R my_lkm_template $proj
+mv $proj/my_lkm_template.c $proj/${proj}.c
+sed -i "s/my_lkm_template/$proj/g" $proj/Makefile
+echo "#Experiment/$proj" > $proj/README.md
