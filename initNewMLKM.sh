3cf99d52c5b6365387be2137bae3aefaf09c63b3
cyng93 <cyng93@gmail.com>
2018-01-09 12:12:33 +0800

    Update MLKM template

--- initNewMLKM.sh
+++ initNewMLKM.sh
@@ -18,6 +18,8 @@ usage()
 # Entry Point
 #
 proj=$1
+Proj=`echo "$proj" | sed -r 's/(^|_)([a-z])/\U\2/g'  `
+PROJ=`echo "$proj" | sed 's/\(.*\)/\U\1/'`
 
 if [ "$#" -lt 1 ]; then
   usage
@@ -29,5 +31,15 @@ if [ -d "$proj" ]; then
 fi
 
 cp -R my_mlkm_template $proj
-sed -i "s/my_mlkm_template/$proj/g" $proj/Makefile
-echo "# Experiment/$proj" > $proj/README.md
+
+cd $proj
+echo "# Experiment/$proj" > README.md
+sed -i "s/my_mlkm_template/$proj/g" Makefile
+mv src/sample.c src/${proj}.c
+mv include/sample.h include/${proj}.h
+
+for f in `find . -type f`; do
+    sed -i "s/sample/${proj}/g" $f
+    sed -i "s/Sample/${Proj}/g" $f
+    sed -i "s/SAMPLE/${PROJ}/g" $f
+done
